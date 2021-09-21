175b9ff1b00143fe5567565eac8f3a90e37204f1
Mariam Gamil <mariam.gamil@live.com>
2021-09-22 00:09:38 +0200

    Push 21-09

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+   public interface IRestaurantData
+    {
+        public IEnumerable<Restaurant> GetAll();
+    }
+
+
+    public class IMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> listOfRestaurant;
+        public IMemoryRestaurantData()
+        {
+            listOfRestaurant = new List<Restaurant>() {
+                new Restaurant{ Id=1,Name="Pizza Hut",Location="Egypt",CuisineType=CuisineType.Italin},
+                new Restaurant{ Id=2,Name="La casta",Location="Egypt",CuisineType=CuisineType.Mexican},
+                new Restaurant{ Id=3,Name="Mahraga",Location="Egypt",CuisineType=CuisineType.India},
+
+                };
+
+
+        }
+        public IEnumerable<Restaurant> GetAll() {
+            return from r in listOfRestaurant
+                   orderby r.Name
+                   select r;
+        }
+
+    }
+}
